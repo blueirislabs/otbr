e1fd315c0af915bab36fb7cfb75a1f128b5a1fc7
Neal Jackson <nealsjackson@gmail.com>
2022-02-02 12:48:16 -0800

    Subprocess to work in python 3.6

--- docker/otbr-watchdog.py
+++ docker/otbr-watchdog.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import subprocess
+from subprocess import PIPE
 
-result = subprocess.run(['ot-ctl', 'state'], capture_output=True)
+result = subprocess.run(['ot-ctl', 'state'], stdout=PIPE, stderr=PIPE)
 state = result.stdout.decode('utf-8').strip().split("\r\n")[0]
 print("Current otbr state: ", state)
-result = subprocess.run(['ot-ctl', 'dataset', 'active'], capture_output=True)
+result = subprocess.run(['ot-ctl', 'dataset', 'active'], stdout=PIPE, stderr=PIPE)
 dataset = "\n\t".join(result.stdout.decode('utf-8').strip().split("\r\n"))
 print("Current otbr dataset: \n", dataset)
 
