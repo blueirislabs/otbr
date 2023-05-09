98de88cd2a8aad54ab9523d14296f2e8ed32db31
Neal Jackson <nealsjackson@gmail.com>
2023-05-08 20:02:50 -0400

    fix activetimestamp

--- otbr_setup.sh
+++ otbr_setup.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-while ! ot-ctl reset
-do
-	echo Waiting for RCP up
-	sleep 5 
-done
+sleep 10
 
 while ! ot-ctl ifconfig up
 do
-	echo Waiting for ifconfig up
-	sleep 5 
+  echo Waiting for ifconfig up
+  sleep 5
 done
 
 ot-ctl thread start
 ot-ctl dataset init new
 ot-ctl dataset networkkey 00112233445566778899aabbccddeeff
+ot-ctl dataset activetimestamp 100
 ot-ctl dataset channel 21
-ot-ctl dataset panid 0xface
-ot-ctl dataset networkname BlueIrisLabs 
+ot-ctl dataset networkname BlueIrisLabs
 ot-ctl dataset commit active
 ot-ctl prefix add fd11:22::/64 pasor
 ot-ctl txpower 2
 ot-ctl netdata register
-
